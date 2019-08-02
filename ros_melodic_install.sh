ea5d2c6cdcd10383729ab1e27ab81c5add0e78f6
Kensuke Saito <ken.saito.0813@gmail.com>
2019-08-02 15:25:04 +0900

    Update ros_melodic_install.sh

--- ros_melodic_install.sh
+++ ros_melodic_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 sudo apt update
-sudo apt upgrade -y
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
@@ -22,4 +21,3 @@ source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 
 sudo apt update
-sudo apt upgrade -y
